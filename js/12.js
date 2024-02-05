d0087d6f89ca31c9fae384f63fd568f23517ff8f
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-02-05 15:21:27 +0000

    commit after restart

--- js/12.js
+++ js/12.js
@@ -1,10 +1,10 @@
 // Надрукувати повну таблицю множення від 1 до 10.
 
-for (x = 1; x <= 10; x++) {
+for (let x = 1; x <= 10; x++) {
     
     console.log(`${x} times table:`)
     
-    for (y = 1; y <= 10; y++) {
+    for (let y = 1; y <= 10; y++) {
         let multRes = x * y;
         console.log(`${x} * ${y} = ${multRes}`);
     }
